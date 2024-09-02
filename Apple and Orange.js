48111d59bb6dd5964ee9f98f04052ef34da9ecbd
vitor1257 <134332194+vitor1257@users.noreply.github.com>
2024-09-02 08:54:57 -0300

    oi

--- Apple and Orange.js
+++ Apple and Orange.js
@@ -20,18 +20,6 @@ function readLine() {
     return inputString[currentLine++];
 }
 
-/*
- * Complete the 'countApplesAndOranges' function below.
- *
- * The function accepts following parameters:
- *  1. INTEGER s
- *  2. INTEGER t
- *  3. INTEGER a
- *  4. INTEGER b
- *  5. INTEGER_ARRAY apples
- *  6. INTEGER_ARRAY oranges
- */
-
 function countApplesAndOranges(s, t, a, b, apples, oranges) {
     let maca = apples.map(num => num + a).filter(num => num >= s && num <= t).length
     let laranja = oranges.map(num => num + b).filter(num => num >= s && num <= t).length
